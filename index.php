34437d4e76408aab8590d6a6946934e48e457f6a
vabanga <gohavorsin@gmail.com>
2017-08-26 09:39:28 +1000

    end

--- index.php
+++ index.php
@@ -27,76 +27,22 @@ $data = mysqli_fetch_all($res);
 				<th>Жанр</th>
 				<th>ISBN</th>
 			</tr>
-			<tr>
-				<td><?php echo $data[0][1]; ?></td>
-				<td><?php echo $data[0][2]; ?></td>
-				<td><?php echo $data[0][3]; ?></td>
-				<td><?php echo $data[0][5]; ?></td>
-				<td><?php echo $data[0][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[1][1]; ?></td>
-				<td><?php echo $data[1][2]; ?></td>
-				<td><?php echo $data[1][3]; ?></td>
-				<td><?php echo $data[1][5]; ?></td>
-				<td><?php echo $data[1][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[2][1]; ?></td>
-				<td><?php echo $data[2][2]; ?></td>
-				<td><?php echo $data[2][3]; ?></td>
-				<td><?php echo $data[2][5]; ?></td>
-				<td><?php echo $data[2][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[3][1]; ?></td>
-				<td><?php echo $data[3][2]; ?></td>
-				<td><?php echo $data[3][3]; ?></td>
-				<td><?php echo $data[3][5]; ?></td>
-				<td><?php echo $data[3][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[4][1]; ?></td>
-				<td><?php echo $data[4][2]; ?></td>
-				<td><?php echo $data[4][3]; ?></td>
-				<td><?php echo $data[4][5]; ?></td>
-				<td><?php echo $data[4][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[5][1]; ?></td>
-				<td><?php echo $data[5][2]; ?></td>
-				<td><?php echo $data[5][3]; ?></td>
-				<td><?php echo $data[5][5]; ?></td>
-				<td><?php echo $data[5][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[6][1]; ?></td>
-				<td><?php echo $data[6][2]; ?></td>
-				<td><?php echo $data[6][3]; ?></td>
-				<td><?php echo $data[6][5]; ?></td>
-				<td><?php echo $data[6][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[7][1]; ?></td>
-				<td><?php echo $data[7][2]; ?></td>
-				<td><?php echo $data[7][3]; ?></td>
-				<td><?php echo $data[7][5]; ?></td>
-				<td><?php echo $data[7][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[8][1]; ?></td>
-				<td><?php echo $data[8][2]; ?></td>
-				<td><?php echo $data[8][3]; ?></td>
-				<td><?php echo $data[8][5]; ?></td>
-				<td><?php echo $data[8][4]; ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $data[9][1]; ?></td>
-				<td><?php echo $data[9][2]; ?></td>
-				<td><?php echo $data[9][3]; ?></td>
-				<td><?php echo $data[9][5]; ?></td>
-				<td><?php echo $data[9][4]; ?></td>
-			</tr>
+			<?php
+			$n = 0;
+			$v1 = [];
+			foreach ($data as $v1) {
+				?>
+				<tr>
+					<td><?=$v1[1]?></td>
+					<td><?=$v1[2]?></td>
+					<td><?=$v1[3]?></td>
+					<td><?=$v1[4]?></td>
+					<td><?=$v1[5]?></td>
+				</tr>
+				<?php
+			}
+
+			?>
 		</tbody>
 	</table>
 </body>
